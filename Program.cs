71014cbbf09bdc358ec6d95c32b4e3a1ffad8a9d
Carson Johnson <carson74johnson@gmail.com>
2021-12-30 09:10:11 -0500

    learned math operations

--- Program.cs
+++ Program.cs
@@ -25,6 +25,21 @@ namespace CatWorx.BadgeMaker
             double area = (side * side);
             Console.WriteLine("area: {0}", area);
             Console.WriteLine("area: {0}", area.GetType());
+
+            // Math Operators
+
+            Console.WriteLine(2 * 3); // 6  // Basic Arithmetic: +, -, /, *
+            Console.WriteLine(10 % 3); // 1  // Modulus Op => remainder of 10/3
+            Console.WriteLine(1 + 2 * 3); // 7  // order of operations
+            Console.WriteLine(10 / 3.0); // 3.333333  // int's and doubles
+            Console.WriteLine(10 / 3); // 3  // int's
+            Console.WriteLine("12" + "3"); // 123  // What happens here?
+
+            int num = 10;
+            num += 100; // 10 + 100 = 110
+            Console.WriteLine(num); // 110
+            num ++; // 110 + 1 = 111
+            Console.WriteLine(num); // 111
         }
     }
 }
