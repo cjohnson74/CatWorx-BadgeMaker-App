8bad7a5f49ac2df3e22b0b32bd369efdd0fa52d9
Carson Johnson <carson74johnson@gmail.com>
2022-01-10 09:37:56 -0500

    added the rest of the variables to the Employee class

--- Employee.cs
+++ Employee.cs
@@ -3,5 +3,8 @@ namespace CatWorx.BadgeMaker
     class Employee
     {
         public string FirstName;
+        public string LastName;
+        public int Id;
+        public string PhotoUrl;
     }
 }
