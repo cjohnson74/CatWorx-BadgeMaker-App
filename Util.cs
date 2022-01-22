53f726e9c3f3f8d1323e5cdb2671c9161ab531d8
Carson Johnson <carson74johnson@gmail.com>
2022-01-22 16:26:54 -0500

    fixed bug in Util

--- Util.cs
+++ Util.cs
@@ -18,28 +18,30 @@ namespace CatWorx.BadgeMaker
             }
         }
 
-        public static void MakeCSV(List<Employee> employees) {
+        public static void MakeCSV(List<Employee> employees)
+        {
             // Check to see if folder exists
             if (!Directory.Exists("data"))
             {
                 // If, not create it
                 Directory.CreateDirectory("data");
-                // This creates new CSV file in the data folder/directory
-                // The using statement manually disposes fo the StreamWriter object, to save memory
-                using (StreamWriter file = new StreamWriter("data/employees.csv"))
-                {
-                    // Any code that needs the StreamWriter would go in here
-                    file.WriteLine("ID,Name,PhotoUrl");
+            }
+            // This creates new CSV file in the data folder/directory
+            // The using statement manually disposes fo the StreamWriter object, to save memory
+            using (StreamWriter file = new StreamWriter("data/employees.csv"))
+            {
+                // Any code that needs the StreamWriter would go in here
+                file.WriteLine("ID,Name,PhotoUrl");
 
-                    // Loop over employees
-                    for (int i = 0; i < employees.Count; i++)
-                    {
-                        // Write each employee to the file
-                        string template = "{0},{1},{2}";
-                        file.WriteLine(String.Format(template, employees[i].GetId(), employees[i].GetName(), employees[i].GetPhotoUrl()));
-                    }
+                // Loop over employees
+                for (int i = 0; i < employees.Count; i++)
+                {
+                    // Write each employee to the file
+                    string template = "{0},{1},{2}";
+                    file.WriteLine(String.Format(template, employees[i].GetId(), employees[i].GetName(), employees[i].GetPhotoUrl()));
                 }
             }
+
         }
     }
 }
