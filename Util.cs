a2783ba4226997eeea082162b7b7349d5ca168c2
Carson Johnson <carson74johnson@gmail.com>
2022-01-24 12:48:57 -0500

    convert the file stream into an image

--- Util.cs
+++ Util.cs
@@ -85,6 +85,7 @@ namespace CatWorx.BadgeMaker
                 for (int i = 0; i < employees.Count; i++)
                 {
                     Stream employeeStream = client.OpenRead(employees[i].GetPhotoUrl());
+                    Image photo = Image.Load(employeeStream);
                 }
             }
         }
